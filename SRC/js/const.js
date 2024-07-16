bd9fc415caac405bdc0103a0648e19614861927d
nomura nao <202404-AP-01-nao-nomura@x-ide.sakura.ne.jp>
2024-07-16 14:07:31 +0900

    Redmineのチケットナンバー #23971
    不具合状況の説明：工事更新_
    （宝物庫8)小数点2桁以内でもエラーメッセージが表示される
    （宝物庫6)仕様書では9桁だが、「販売予定額は8桁以内の半角数字で入力してください」とのエラーが表示される
    
    修正内容の説明：js/const.js内
    （宝物庫8) if (tmp.length > 6 || tmp.match(/[^0-9]+/)) {を if (tmp.length > 0 && !tmp.match(/^([0-9]{1,3})(\.[0-9]{1,2})?$/)) {に変更
    （宝物庫6） if (isNumericLength(8, "販売予定額", form.sellPrice))を  if (isNumericLength(9, "販売予定額", form.sellPrice)) { return; }に変更

--- SRC/js/const.js
+++ SRC/js/const.js
@@ -3,13 +3,13 @@
 //
 function fnConstEditCheck() {
 	tmp = form.area.value;
-	if (tmp.length > 6 || tmp.match(/[^0-9]+/)) {
+	if (tmp.length > 0 && !tmp.match(/^([0-9]{1,3})(\.[0-9]{1,2})?$/)) {
 		alert('面積は3桁以内（小数点以下2桁以内）の半角数字で入力してください');
 		return;
 	}
 
 	if (isLength(100, "築年", form.years)) { return; }
-	if (isNumericLength(8, "販売予定額", form.sellPrice)) { return; }
+	if (isNumericLength(9, "販売予定額", form.sellPrice)) { return; }
 	if (isNumericLength(9, "内装見越額", form.interiorPrice)) { return; }
 	if (isLength(100, "施工業者", form.constTrader)) { return; }
 	if (isNumericLength(9, "工事金額", form.constPrice)) { return; }
